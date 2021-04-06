354da8e871592fc1f41276d72fe2fe2e9c955c60
Ayushi <ayushichtrvd@gmail.com>
2021-04-06 20:46:12 +0530

    1st-April-2021

--- src/App.js
+++ src/App.js
@@ -1,65 +1,59 @@
-import React ,{Component} from 'react' ;
-import { Route, Switch, withRouter, Redirect  } from 'react-router-dom';
-import { connect} from 'react-redux';
+import React, { useEffect, Suspense } from 'react';
+import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import Layout from './hoc/Layout/Layout';
-import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder' ;  
-import Checkout from './containers/Checkout/Checkout';
-import Orders from './containers/Orders/Orders';
-import Auth from './containers/Auth/Auth';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Logout from './containers/Auth/Logout/Logout';
 import * as actions from './store/actions/index';
-import asyncComponent from './hoc/asyncComponent/asyncComponent';
 
-
-const asyncCheckout = asyncComponent(() => {
+const Checkout = React.lazy(() => {
   return import('./containers/Checkout/Checkout');
 });
 
-const asyncOrders = asyncComponent(() => {
+const Orders = React.lazy(() => {
   return import('./containers/Orders/Orders');
 });
 
-const asyncAuth = asyncComponent(() => {
+const Auth = React.lazy(() => {
   return import('./containers/Auth/Auth');
 });
 
-class App extends Component {
-  componentDidMount () {
-    this.props.onTryAutoSignup();
-  }
+const App = props => {
+  const { onTryAutoSignup } = props;
+  useEffect(() => {
+    onTryAutoSignup();
+  }, [onTryAutoSignup]);
 
-  render(){
-    let routes = (
-      <Switch>
-      <Route path="/auth" component={Auth} />
+  let routes = (
+    <Switch>
+      <Route path="/auth" render={(props) => <Auth{...props}/>} />
       <Route path="/" exact component={BurgerBuilder} />
       <Redirect to="/" />
+    </Switch>
+  );
+
+  if (props.isAuthenticated) {
+    routes = (
+      <Switch>
+        <Route path="/checkout" render={(props) => <Checkout {...props} />} />
+        <Route path="/orders" render={(props) => <Orders {...props} />} />
+        <Route path="/logout" component={Logout} />
+        <Route path="/auth" render={(props) => <Auth {...props} />} />
+        <Route path="/" exact component={BurgerBuilder} />
+        <Redirect to="/" />
       </Switch>
     );
-    if (this.props.isAuthenticated) {
-      routes = (
-        <Switch>
-            <Route path="/checkout" component={Checkout} />
-            <Route path="/orders" component={Orders} />
-            <Route path="/logout" component={Logout} />
-            <Route path="/auth" component={Auth} />
-            <Route path="/" exact component={BurgerBuilder} /> 
-            <Redirect to="/" />
-        </Switch>
-      );
-    }
+  }
 
   return (
-    <div >
+    <div>
       <Layout>
-        {routes}
-        </Layout >
-      
+        <Suspense fallback={<p>Loading...</p>}>{routes}</Suspense>
+      </Layout>
     </div>
   );
-}
-}
+};
 
 const mapStateToProps = state => {
   return {
@@ -73,4 +67,9 @@ const mapDispatchToProps = dispatch => {
   };
 };
 
-export default withRouter(connect(mapStateToProps , mapDispatchToProps)(App));
+export default withRouter(
+  connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )(App)
+);
--- src/components/Burger/OderSummary/OrderSummary.js
+++ src/components/Burger/OderSummary/OrderSummary.js
@@ -1,16 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import Aux from '../../../hoc/Aux/Aux';
 import Button from '../../UI/Button/Button';
 
-class OrderSummary extends Component{
+const OrderSummary = props => {
     
-    render(){
 
-        const ingredientSummary = Object.keys(this.props.ingredients)
+
+        const ingredientSummary = Object.keys(props.ingredients)
         .map(igKey => {
             return (<li>
-                <span style={{textTransform:'capitalize'}}> {igKey}</span>: {this.props.ingredients[igKey]}
+                <span style={{textTransform:'capitalize'}}> {igKey}</span>: {props.ingredients[igKey]}
                 </li>);
         });
 
@@ -21,13 +21,13 @@ class OrderSummary extends Component{
         <ul>
             {ingredientSummary}
         </ul>
-        <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
+        <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
         <p>Continue to checkout?</p>
-        <Button btnType='Danger'clicked={this.props.purchaseCancelled}>CANCEL</Button>
-        <Button btnType='Success' clicked={this.props.purchaseContinued}>CONTINUE</Button>
+        <Button btnType='Danger'clicked={props.purchaseCancelled}>CANCEL</Button>
+        <Button btnType='Success' clicked={props.purchaseContinued}>CONTINUE</Button>
     </Aux>
         );
-    }
+
 } 
 
 export default OrderSummary;
